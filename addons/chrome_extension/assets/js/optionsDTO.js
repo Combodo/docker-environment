dbb9e6259acf22def8f7608c5f193ca62340e100
Benjamin DALSASS <benjamin.dalsass@combodo.com>
2026-02-13 07:15:32 +0100

    :bookmark: Chrome extension version 1.2 with php 8.5 bundled

--- addons/chrome_extension/assets/js/optionsDTO.js
+++
@@ -1,6 +0,0 @@
-const optionsDTO = {
-    php_version: '82',
-    url_filter: 'localhost',
-    enabled: 'true',
-    php_version_installed: '{74: "7.4",80: "8.0",81: "8.1",82: "8.2",83: "8.3",84: "8.4"}'
-};
